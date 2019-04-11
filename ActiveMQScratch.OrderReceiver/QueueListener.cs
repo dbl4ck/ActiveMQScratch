b6e3788be414c9483ace2e8ffc28ca6dceb5ce5f
dbl4k <github@dbl4k.net>
2019-04-11 23:49:41 +0100

    upgrade bootstrap to v4

---
+++ ActiveMQScratch.OrderReceiver/QueueListener.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ.Commands;
+using Apache.NMS.Util;
+
+namespace ActiveMQScratch.OrderReceiver
+{
+    class MqListener
+    {
+        private IMessageConsumer _consumer;
+
+        public MqListener(IMessageConsumer consumeer, string queueName)
+        {
+            _session = this.session;
+            _destination = destina
+        }
+        
+    }
+}
